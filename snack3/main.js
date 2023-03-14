ed63051d1d2750f2d8997491bc1678779851bf59
Richter96 <castiglione96.rc@gmail.com>
2023-03-14 12:38:03 +0100

    snack3

---
+++ snack3/main.js
@@ -0,0 +1,13 @@
+/* Snack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+
+
+let userNumber;
+
+for (i = 0; i < 10; i++) {
+    userNumber = prompt('dichiara 10 numeri')
+    console.log(userNumber)
+}
