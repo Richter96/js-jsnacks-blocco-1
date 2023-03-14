76421e5204a13d4b56ecbc8a2d4956a96ced3069
Richter96 <castiglione96.rc@gmail.com>
2023-03-14 12:24:01 +0100

    add second and first word

--- snack2/main.js
+++ snack2/main.js
@@ -5,6 +5,18 @@ Il software stampa prima la parola più corta, poi la parola più lunga. */
 const firstWord = prompt('insrisci una parola')
 const secondWord = prompt('insrisci una parola')
 
-console.log(firstWord.length)
+console.log(firstWord)
+console.log(secondWord)
 
-if (firstWord.length >)
+const longFirstWord = firstWord.length
+console.log(longFirstWord)
+
+const longSecondWord = secondWord.length
+console.log(longSecondWord)
+
+if (longFirstWord > longSecondWord) {
+    console.log(`la parola piu corta è di ${longSecondWord} carattteri ed è ${secondWord} mentre la piu lunga di ${longFirstWord} caratteri ed è ${firstWord}`)
+} else if (longSecondWord > longFirstWord) {
+    console.log(`la parola piu corta è di ${longFirstWord} carattteri ed è ${firstWord} mentre la piu lunga è la seconda parola di ${longSecondWord} caratteri ed è ${secondWord}`)
+} 
+
