ad022200f60c3b5f3c13dad09056a041fdf0ce4f
Richter96 <castiglione96.rc@gmail.com>
2023-03-15 12:50:26 +0100

    add while

--- snack5/main.js
+++ snack5/main.js
@@ -9,8 +9,6 @@ const numDisp = [
 
 ]
 
-
-
 for (let i = 0; i < 6; i++) {
     // chiediamo un numero all'utente per 6 volte
     const userNumb = Number(prompt('dichiara un numero'))
@@ -23,3 +21,18 @@ for (let i = 0; i < 6; i++) {
 }
 
 
+// while
+let k = 0
+while (k < 6) {
+    // chiediamo un numero all'utente per 6 volte
+    const userNumb = Number(prompt('dichiara un numero'))
+    // creo un if per trovare i numeri dispari
+    if (userNumb % 2 != 0) {
+        numDisp.push(userNumb)
+        console.log(userNumb)
+        console.log('il numero è dispari');
+    }
+    i++
+}
+
+console.log()
