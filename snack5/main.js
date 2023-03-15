49cedb360f4dac3d1c1fe94f4092c265a6882814
Richter96 <castiglione96.rc@gmail.com>
2023-03-15 12:43:35 +0100

    add cicle number

---
+++ snack5/main.js
@@ -0,0 +1,25 @@
+/* Snack 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. */
+
+
+
+const numDisp = [
+
+]
+
+
+
+for (let i = 0; i < 6; i++) {
+    // chiediamo un numero all'utente per 6 volte
+    const userNumb = Number(prompt('dichiara un numero'))
+    // creo un if per trovare i numeri dispari
+    if (userNumb % 2 != 0) {
+        numDisp.push(userNumb)
+        console.log(userNumb)
+        console.log('il numero è dispari');
+    }
+}
+
+
